84b987be505528f4092fcf364dd73136b7f71334
Philip Yoo <philipyoo10@gmail.com>
2015-11-01 14:46:06 -0800

    Edit board creation of pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 
-
+require_relative 'piece'
 
 class Board
   attr_reader :board
@@ -16,15 +16,30 @@ class Board
 
   private
 
-  def setup_board
-    rows = [0, 1, 6, 7]
+  def back_rows(color)
+    back_pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
 
-    8.times do |i|
-      if rows.include?(i)
-        @board[i].map! { TestPiece.new }
-      else
-        @board[i].map! { EmptyPiece.new }
-      end
+    i = (color == :white) ? 7 : 0
+    back_pieces.each_with_index do |piece, j|
+      puts "*" * 10
+      puts self
+      piece.new(color, [i, j])
+    end
+
+  end
+
+  def pawn_rows(color)
+    i = (color == :white) ? 6 : 1
+    8.times do |j|
+      Pawn.new(color, [i, j])
+    end
+  end
+
+  def setup_board
+    [:white, :black].each do |color|
+      puts 'hi'
+      back_rows(color)
+      pawn_rows(color)
     end
   end
 
